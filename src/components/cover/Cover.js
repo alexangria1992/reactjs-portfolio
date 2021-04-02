75970789048b1038c5cc3c91bf55ed0be19c789c
alexangria1992 <alex040892@gmail.com>
2021-04-02 21:06:04 +1300

    Changed cover font

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -4,8 +4,10 @@ import coverVideo from "../../media/coverVideo.mp4"
 
 const Cover = () => {
     return (
-        <div>
+        <div className="cover-container">
             <video className="video" src={coverVideo} autoPlay loop muted/>
+            <h1>Alexandre Angrignon</h1>
+            <p>Frontend Developer</p>
         </div>
     )
 }
