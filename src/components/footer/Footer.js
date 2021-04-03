314f2dad6e5d5364df89de10a86f2b5be8966ed2
alexangria1992 <alex040892@gmail.com>
2021-04-03 15:17:46 +1300

    Responsive Media Queries

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,19 +14,18 @@
                 <br />
                 <p>Mobile: 021-254-2572 </p>
                 <p>Email: alex040892@gmail.com </p>
-
             </div>
             <div className="footer-sns">
                 <div className="design-by">Designed By Alexandre Angrignon</div>
                 <div className="sns-links">
                     <a href="https://www.facebook.com/alexandre.angrignon.7/" target="_blank" rel="noreferrer">
-                         <i class="fab fa-facebook facebook"></i>
+                         <i className="fab fa-facebook facebook"></i>
                     </a>
                     <a href="https://twitter.com/92_Angryman" target="_blank" rel="noreferrer">
-                         <i class="fab fa-twitter twitter"></i>
+                         <i className="fab fa-twitter twitter"></i>
                     </a>
                     <a href="https://github.com/alexangria1992" target="_blank" rel="noreferrer">
-                          <i class="fab fa-github github"></i>                   
+                          <i className="fab fa-github github"></i>                   
                      </a>
                 </div>
             </div>
