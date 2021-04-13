19561641135ddfba3da978d607b0748c3050a0ab
alexangria1992 <alex040892@gmail.com>
2021-04-13 15:30:37 +1200

    Updated portfolio github link

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -5,25 +5,56 @@ const slidesInfo = [
         src: "https://i.imgur.com/mC8103h.jpg",
         alt: "Project 1",
         desc: "Project 1",
-        link: "https://proshop-demonstration.herokuapp.com/#/"
+        link: "https://proshop-demonstration.herokuapp.com/#/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/django-react-pro-shop"
     }, 
     {
         src: "https://i.imgur.com/qhcghIR.jpg",
         alt: "Project 2",
         desc: "Project 2",
-        link: "https://boomerang-dominion.herokuapp.com/"
+        link: "https://boomerang-dominion.herokuapp.com/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/Boomerang-Website"
+
     },
     {
         src: "https://i.imgur.com/UZdEyOe.jpg",
         alt: "Project 3",
         desc: "Project 3",
-        link: "https://dolla-website.herokuapp.com/"
+        link: "https://dolla-website.herokuapp.com/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/dolla-website"
+
     },
     {
         src: "https://i.imgur.com/gIExML4.jpg",
         alt: "Project 4",
         desc: "Project 4",
-        link: "https://capture-site.herokuapp.com/"
+        link: "https://capture-site.herokuapp.com/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/react-capture"
+
+
+    },
+    {
+        src: "https://i.imgur.com/VsVEQaz.jpg?1",
+        alt: "Project 5",
+        desc: "Project 5",
+        link: "https://employee-mui-app.herokuapp.com/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/react-mui-system"
+
+
+    },
+    {
+        src: "https://i.imgur.com/RztMOUF.jpg",
+        alt: "Project 6",
+        desc: "Project 6",
+        link: "https://easybank-website.herokuapp.com/",
+        icon: "fab fa-github fa-3x",
+        iconLink: "https://github.com/alexangria1992/fem-easybank"
+
 
     },
         
@@ -36,6 +67,9 @@ const slides = slidesInfo.map(slide => (
         </a>
         <div className="slide-desc">
             <span>{slide.desc}</span>
+            <a href={slide.iconLink} target="_blank">
+                <i className={slide.icon}></i>
+            </a>
         </div>
     </div>
 ))
